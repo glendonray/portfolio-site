2323b1251f36bccec9e3ec878734e938a7325711
Glendon Grapperhaus <glendonray@gmail.com>
2024-03-04 13:42:10 -0600

    Initial Commit for Rework

---
+++ gg-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="gg-portfolio">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
