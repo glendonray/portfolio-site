af5a78e640b769f9b82ad972359ea1fe050dba57
Glendon Grapperhaus <glendonray@gmail.com>
2024-07-25 15:25:57 -0500

    Intro Section Update

--- gg-portfolio/src/components/Intro.js
+++ gg-portfolio/src/components/Intro.js
@@ -7,13 +7,16 @@ class Intro extends React.Component {
         <div className="intro-wrapper">
           <div className="intro">
             <div className="intro-text">
-              <h2 className="career-title">Software Engineer</h2>
+              <h2 className="career-title">
+                Web Developer &amp; Software Engineer
+              </h2>
               <p className="summary">
-                Full stack engineer with over ten years of experience in
-                creating beautiful websites. Proficient in translating designs
-                into pixel-perfect user interfaces, lending to wonderful user
-                experiences. Excellent soft skills with a love for communicating
-                and collaborating with a team.
+                Full-stack developer and engineer with over ten years of
+                experience in writing clean code and creating beautiful
+                websites. Proficient in translating designs into pixel-perfect
+                user interfaces, lending to wonderful user experiences.
+                Excellent soft skills with a love for communicating and
+                collaborating with a team.
               </p>
             </div>
             <div className="portrait">
